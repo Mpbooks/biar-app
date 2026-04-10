6aab8c1401e5381e64620556ef2cef7cdb30abd7
Paulinho <159821950+Mpbooks@users.noreply.github.com>
2026-04-10 20:25:58 -0300

    Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useLanguage } from '../context/LanguageContext'
 import { login as loginApi } from '../api/auth'
 import GridDistortion from '../components/GridDistortion';
-import { degrees } from 'motion';
 
 const styles = `
   @import url('https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Mono:wght@300;400;500&display=swap');
